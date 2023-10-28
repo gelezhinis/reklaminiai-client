732ab7b58229c617503be3ff3e0e5f166a86ecf3
Linas Geležauskas <gelezhinis@gmail.com>
2023-10-28 17:02:55 +0300

    will work

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -21,7 +21,7 @@ function MainNavigation() {
         <nav>
           <ul>
             <li>
-              <Link href={'/produktai'}>Produkteeeeeeeeeliai</Link>
+              <Link href={'/produktai'}>Produktai</Link>
             </li>
             <li>
               <Link href={'/kontaktai'}>Kontaktai</Link>
